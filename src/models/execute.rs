c5539eb958193f1504bde7718d33974a9f841347
div-styl <gouhmada@gmail.com>
2023-08-23 22:54:27 +0100

    the home dir is handled succefully

--- src/models/execute.rs
+++ src/models/execute.rs
@@ -1,7 +1,7 @@
-use color_print::cprintln;
-use std::process::Command;
-
 use crate::models::dashboard::prompt;
+use color_print::cprintln;
+use std::env;
+use std::process::Command;
 
 /**
  * * execmd - function that execute the command
@@ -15,6 +15,23 @@ pub fn execmd(cmdline: &str, argu: &[String]) {
     match cmdline {
         "" => {
             // *empty input do nothing!
+        }
+        "cd" => {
+            let home = env::var("HOME").unwrap_or_default(); // fectch the home dir
+            if argu.len() == 1 {
+                if &argu[0] == "~" {
+                    cprintln!("<green><bold>Changing to Home Directory</>");
+                    if let Err(err) = env::set_current_dir(&home) {
+                        prompt(false);
+                        cprintln!("<red><bold>Error: Directory Change Failed: {}</>", err);
+                    }
+                } else {
+                    if let Err(err) = env::set_current_dir(&argu[0]) {
+                        prompt(false);
+                        cprintln!("<red><bold>Error: Directory Change Failed 2: {}</>", err);
+                    }
+                }
+            }
         }
         _ => {
             // Handle non-exit command here
